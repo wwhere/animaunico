8abec622460ecf3a14a6e0a0196cf1bdab34c506
Guillermo Heras <guillermo.heras@gmail.com>
2014-04-02 00:33:37 +0200

    iniciar modelo de invocaciones y listar las del basico
    re #104

---
+++ js/model/Invocacion.js
@@ -0,0 +1,19 @@
+/**
+ *
+ * @param {string} nombre
+ * @param {number} grado
+ * @constructor
+ */
+function Invocacion(nombre, grado) {
+    /**
+     *
+     * @type {string}
+     */
+    this.nombre = nombre;
+
+    /**
+     *
+     * @type {number}
+     */
+    this.grado = grado;
+}
