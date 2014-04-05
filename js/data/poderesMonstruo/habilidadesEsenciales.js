f432eb4d70c0fbeb5334c9b8dbb651549c3f8df8
Guillermo Heras <guillermo.heras@gmail.com>
2014-04-05 23:28:34 +0200

    Introducir habilidades esenciales y podres
    re #1

--- js/data/poderesMonstruo/habilidadesEsenciales.js
+++ js/data/poderesMonstruo/habilidadesEsenciales.js
@@ -1,9 +1,12 @@
 var habilidadesEsenciales_set = [];
 var allHabilidadesEsenciales = {};
 
+var desventajasEsenciales_set = [];
+var allDesventajasEsenciales = {};
+
 var habilidadEsencial;
 
-function HabilidadEsencial(nombre,descripcion,coste,gnosis, repetible, incompatibles, elegir, efecto) {
+function HabilidadEsencial(nombre,descripcion,coste,gnosis, repetible, incompatibles, elegir, efecto, grupo ) {
     this.nombre = nombre;
     this.descripcion = descripcion;
     this.coste = coste;
@@ -12,6 +15,9 @@ function HabilidadEsencial(nombre,descripcion,coste,gnosis, repetible, incompati
     this.incompatibles = incompatibles;
     this.elegir = elegir;
     this.efecto = efecto;
+    if (!grupo)
+        grupo = GRUPO_OTRAS;
+    this.grupo = grupo;
 }
 
 function hsResistenciaAlCansancio(coste, opcion,aplicar) {
@@ -526,3 +532,509 @@ habilidadEsencial = new HabilidadEsencial(
 );
 habilidadesEsenciales_set.push(habilidadEsencial);
 allHabilidadesEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Recuperación superior de magia",
+    "",
+    30,
+    5,
+    NO_REPETIBLE,
+    [],
+    [],
+    [],
+    GRUPO_DON
+);
+habilidadesEsenciales_set.push(habilidadEsencial);
+allHabilidadesEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Conocimiento natural de una vía 20",
+    "",
+    10,
+    10,
+    REPETIBLE_OPCIONES,
+    [],
+    new Array(LISTA_VIAS_MAGIA),
+    new Array(conocimientoNaturalVia),
+    GRUPO_DON
+);
+habilidadesEsenciales_set.push(habilidadEsencial);
+allHabilidadesEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Conocimiento natural de una vía 40",
+    "",
+    20,
+    15,
+    REPETIBLE_OPCIONES,
+    [],
+    new Array(LISTA_VIAS_MAGIA),
+    new Array(conocimientoNaturalVia),
+    GRUPO_DON
+);
+habilidadesEsenciales_set.push(habilidadEsencial);
+allHabilidadesEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Conocimiento natural de una vía 60",
+    "",
+    30,
+    20,
+    REPETIBLE_OPCIONES,
+    [],
+    new Array(LISTA_VIAS_MAGIA),
+    new Array(conocimientoNaturalVia),
+    GRUPO_DON
+);
+habilidadesEsenciales_set.push(habilidadEsencial);
+allHabilidadesEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Conocimiento natural de una vía 80",
+    "",
+    40,
+    25,
+    REPETIBLE_OPCIONES,
+    [],
+    new Array(LISTA_VIAS_MAGIA),
+    new Array(conocimientoNaturalVia),
+    GRUPO_DON
+);
+habilidadesEsenciales_set.push(habilidadEsencial);
+allHabilidadesEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Conocimiento natural de una vía 90",
+    "",
+    50,
+    30,
+    REPETIBLE_OPCIONES,
+    [],
+    new Array(LISTA_VIAS_MAGIA),
+    new Array(conocimientoNaturalVia),
+    GRUPO_DON
+);
+habilidadesEsenciales_set.push(habilidadEsencial);
+allHabilidadesEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Inutilidad somática",
+    "",
+    20,
+    5,
+    NO_REPETIBLE,
+    [],
+    [],
+    [],
+    GRUPO_DON
+);
+habilidadesEsenciales_set.push(habilidadEsencial);
+allHabilidadesEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Inutilidad oral",
+    "",
+    20,
+    5,
+    NO_REPETIBLE,
+    [],
+    [],
+    [],
+    GRUPO_DON
+);
+habilidadesEsenciales_set.push(habilidadEsencial);
+allHabilidadesEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Magia innata mejorada",
+    "",
+    20,
+    5,
+    NO_REPETIBLE,
+    [],
+    [],
+    [],
+    GRUPO_DON
+);
+habilidadesEsenciales_set.push(habilidadEsencial);
+allHabilidadesEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Mantenimiento añadido",
+    "",
+    30,
+    5,
+    NO_REPETIBLE,
+    [],
+    [],
+    [],
+    GRUPO_PSIQUICAS
+);
+habilidadesEsenciales_set.push(habilidadEsencial);
+allHabilidadesEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+
+habilidadEsencial = new HabilidadEsencial(
+    "Resistencia a la fatiga psíquica",
+    "",
+    30,
+    5,
+    NO_REPETIBLE,
+    [],
+    [],
+    [],
+    GRUPO_PSIQUICAS
+);
+habilidadesEsenciales_set.push(habilidadEsencial);
+allHabilidadesEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Recuperación rápida de CV",
+    "",
+    20,
+    5,
+    NO_REPETIBLE,
+    [],
+    [],
+    [],
+    GRUPO_PSIQUICAS
+);
+habilidadesEsenciales_set.push(habilidadEsencial);
+allHabilidadesEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Concentración extrema",
+    "",
+    30,
+    5,
+    NO_REPETIBLE,
+    [],
+    [],
+    [],
+    GRUPO_PSIQUICAS
+);
+habilidadesEsenciales_set.push(habilidadEsencial);
+allHabilidadesEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Calibre",
+    "",
+    20,
+    5,
+    NO_REPETIBLE,
+    [],
+    [],
+    [],
+    GRUPO_PSIQUICAS
+);
+habilidadesEsenciales_set.push(habilidadEsencial);
+allHabilidadesEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Desequilibrio psíquico",
+    "",
+    20,
+    5,
+    NO_REPETIBLE,
+    [],
+    [],
+    [],
+    GRUPO_PSIQUICAS
+);
+habilidadesEsenciales_set.push(habilidadEsencial);
+allHabilidadesEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Concentración pasiva",
+    "",
+    30,
+    5,
+    NO_REPETIBLE,
+    [],
+    [],
+    [],
+    GRUPO_PSIQUICAS
+);
+habilidadesEsenciales_set.push(habilidadEsencial);
+allHabilidadesEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Vicio racial",
+    "",
+    10,
+    0,
+    REPETIBLE_OPCIONES,
+    [],
+    [],
+    [],
+    GRUPO_OTRAS
+);
+desventajasEsenciales_set.push(habilidadEsencial);
+allDesventajasEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Miembros atrofiados",
+    "",
+    20,
+    0,
+    NO_REPETIBLE,
+    [],
+    [],
+    [],
+    GRUPO_OTRAS
+);
+desventajasEsenciales_set.push(habilidadEsencial);
+allDesventajasEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+
+habilidadEsencial = new HabilidadEsencial(
+    "Miedo racial",
+    "",
+    10,
+    10,
+    REPETIBLE_OPCIONES,
+    [],
+    [],
+    [],
+    GRUPO_OTRAS
+);
+desventajasEsenciales_set.push(habilidadEsencial);
+allDesventajasEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Terror racial",
+    "",
+    20,
+    10,
+    REPETIBLE_OPCIONES,
+    [],
+    [],
+    [],
+    GRUPO_OTRAS
+);
+desventajasEsenciales_set.push(habilidadEsencial);
+allDesventajasEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Carencia de un sentido",
+    "",
+    10,
+    0,
+    REPETIBLE_OPCIONES,
+    [],
+    [],
+    [],
+    GRUPO_OTRAS
+);
+desventajasEsenciales_set.push(habilidadEsencial);
+allDesventajasEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Ciego",
+    "",
+    20,
+    0,
+    NO_REPETIBLE,
+    [],
+    [],
+    [],
+    GRUPO_OTRAS
+);
+desventajasEsenciales_set.push(habilidadEsencial);
+allDesventajasEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Necesidad física",
+    "",
+    10,
+    0,
+    REPETIBLE_OPCIONES,
+    [],
+    [],
+    [],
+    GRUPO_OTRAS
+);
+desventajasEsenciales_set.push(habilidadEsencial);
+allDesventajasEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Necesidad extrema",
+    "",
+    20,
+    10,
+    REPETIBLE_OPCIONES,
+    [],
+    [],
+    [],
+    GRUPO_OTRAS
+);
+desventajasEsenciales_set.push(habilidadEsencial);
+allDesventajasEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Vulnerabilidad elemental (50% más de daño)",
+    "",
+    10,
+    20,
+    REPETIBLE_OPCIONES,
+    [],
+    [], //TODO lista elementos
+    [],
+    GRUPO_OTRAS
+);
+desventajasEsenciales_set.push(habilidadEsencial);
+allDesventajasEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Vulnerabilidad elemental (doble daño)",
+    "",
+    20,
+    20,
+    REPETIBLE_OPCIONES,
+    [],
+    [],
+    [],
+    GRUPO_OTRAS
+);
+desventajasEsenciales_set.push(habilidadEsencial);
+allDesventajasEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Vulnerable a un tipo de ataque",
+    "",
+    20,
+    10,
+    REPETIBLE_OPCIONES,
+    [],
+    [],
+    [],
+    GRUPO_OTRAS
+);
+desventajasEsenciales_set.push(habilidadEsencial);
+allDesventajasEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Obligación oral",
+    "",
+    10,
+    5,
+    NO_REPETIBLE,
+    [],
+    [],
+    [],
+    GRUPO_DON
+);
+desventajasEsenciales_set.push(habilidadEsencial);
+allDesventajasEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Obligación somática",
+    "",
+    10,
+    5,
+    NO_REPETIBLE,
+    [],
+    [],
+    [],
+    GRUPO_DON
+);
+desventajasEsenciales_set.push(habilidadEsencial);
+allDesventajasEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Lenta recuperación de magia",
+    "",
+    10,
+    5,
+    NO_REPETIBLE,
+    [],
+    [],
+    [],
+    GRUPO_DON
+);
+desventajasEsenciales_set.push(habilidadEsencial);
+allDesventajasEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Magia estanca",
+    "",
+    30,
+    5,
+    NO_REPETIBLE,
+    [],
+    [],
+    [],
+    GRUPO_DON
+);
+desventajasEsenciales_set.push(habilidadEsencial);
+allDesventajasEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Extenuación mágica",
+    "",
+    10,
+    5,
+    NO_REPETIBLE,
+    [],
+    [],
+    [],
+    GRUPO_DON
+);
+desventajasEsenciales_set.push(habilidadEsencial);
+allDesventajasEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Brujería / Magia atada a acción",
+    "",
+    20,
+    5,
+    NO_REPETIBLE,
+    [],
+    [],
+    [],
+    GRUPO_DON
+);
+desventajasEsenciales_set.push(habilidadEsencial);
+allDesventajasEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Sin concentración",
+    "",
+    10,
+    5,
+    NO_REPETIBLE,
+    [],
+    [],
+    [],
+    GRUPO_PSIQUICAS
+);
+desventajasEsenciales_set.push(habilidadEsencial);
+allDesventajasEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Extenuación psíquica",
+    "",
+    10,
+    5,
+    NO_REPETIBLE,
+    [],
+    [],
+    [],
+    GRUPO_PSIQUICAS
+);
+desventajasEsenciales_set.push(habilidadEsencial);
+allDesventajasEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
+
+habilidadEsencial = new HabilidadEsencial(
+    "Consumición física",
+    "",
+    20,
+    5,
+    NO_REPETIBLE,
+    [],
+    [],
+    [],
+    GRUPO_PSIQUICAS
+);
+desventajasEsenciales_set.push(habilidadEsencial);
+allDesventajasEsenciales[habilidadEsencial.nombre] = habilidadEsencial;
---
+++ js/data/poderesMonstruo/poderesMonstruo.js
@@ -0,0 +1,191 @@
+poder = new PoderMonstruo(
+    "Armas naturales", //nombre
+    [
+        {
+            nombre : "Armas naturales", //nombre
+            coste : 20, //coste
+            gnosis: 0, //gnosis
+            efecto: [] //efecto
+        }
+    ]
+);
+
+poder = new PoderMonstruo(
+    "Ataques adicionales", //nombre
+    [
+        {
+            nombre: 'Ataque adicional con –60 a su HA final',
+            coste: 20,
+            gnosis: 0,
+            efecto: []
+        },
+        {
+            nombre: 'Ataque adicional con –50 a su HA final',
+            coste: 30,
+            gnosis: 0,
+            efecto: []
+        },
+        {
+            nombre: 'Ataque adicional con –40 a su HA final',
+            coste: 40,
+            gnosis: 0,
+            efecto: []
+        },
+        {
+            nombre: 'Ataque adicional con –30 a su HA final',
+            coste: 50,
+            gnosis: 5,
+            efecto: []
+        },
+        {
+            nombre: 'Ataque adicional con –20 a su HA final',
+            coste: 60,
+            gnosis: 5,
+            efecto: []
+        },
+        {
+            nombre: 'Ataque adicional con –10 a su HA final',
+            coste: 80,
+            gnosis: 10,
+            efecto: []
+        },
+        {
+            nombre: 'Ataque adicional completo',
+            coste: 100,
+            gnosis: 15,
+            efecto: []
+        }
+    ]
+);
+
+poder = new PoderMonstruo(
+    "Daño incrementado", //nombre
+    [
+        {
+            nombre: '+10 al daño',
+            coste: 10,
+            gnosis: 0,
+            efecto: []
+        },
+        {
+            nombre: '+20 al daño',
+            coste: 20,
+            gnosis: 0,
+            efecto: []
+        },
+        {
+            nombre: '+30 al daño',
+            coste: 30,
+            gnosis: 0,
+            efecto: []
+        },
+        {
+            nombre: '+40 al daño',
+            coste: 40,
+            gnosis: 0,
+            efecto: []
+        },
+        {
+            nombre: '+50 al daño',
+            coste: 50,
+            gnosis: 5,
+            efecto: []
+        },
+        {
+            nombre: '+60 al daño',
+            coste: 60,
+            gnosis: 10,
+            efecto: []
+        },
+        {
+            nombre: '+80 al daño',
+            coste: 80,
+            gnosis: 20,
+            efecto: []
+        },
+        {
+            nombre: '+100 al daño',
+            coste: 100,
+            gnosis: 25,
+            efecto: []
+        },
+        {
+            nombre: '+120 al daño',
+            coste: 120,
+            gnosis: 30,
+            efecto: []
+        }
+    ]
+);
+
+poder = new PoderMonstruo(
+    "Reacción incrementada", //nombre
+    [
+        {
+            nombre: '+10 al turno natural',
+            coste: 10,
+            gnosis: 0,
+            efecto: []
+        },
+        {
+            nombre: '+20 al turno natural',
+            coste: 20,
+            gnosis: 10,
+            efecto: []
+        },
+        {
+            nombre: '+30 al turno natural',
+            coste: 30,
+            gnosis: 20,
+            efecto: []
+        }
+    ]
+);
+
+poder = new PoderMonstruo(
+    "Dañar energía", //nombre
+    [
+        {
+            nombre : "Dañar energía", //nombre
+            coste : 10, //coste
+            gnosis: 10, //gnosis
+            efecto: [] //efecto
+        }
+    ]
+);
+
+poder = new PoderMonstruo(
+    "Modificador de armadura", //nombre
+    [
+        {
+            nombre: '-1 a la TA defensora',
+            coste: 10,
+            gnosis: 0,
+            efecto: []
+        },
+        {
+            nombre: '-2 a la TA defensora',
+            coste: 15,
+            gnosis: 10,
+            efecto: []
+        },
+        {
+            nombre: '-3 a la TA defensora',
+            coste: 20,
+            gnosis: 15,
+            efecto: []
+        },
+        {
+            nombre: '-4 a la TA defensora',
+            coste: 25,
+            gnosis: 20,
+            efecto: []
+        },
+        {
+            nombre: '-5 a la TA defensora',
+            coste: 30,
+            gnosis: 25,
+            efecto: []
+        }
+    ]
+);
