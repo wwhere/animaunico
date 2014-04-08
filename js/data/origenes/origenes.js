66a76ab8dd89ee73e2713d960ec129d7ae72bb1d
Guillermo Heras <guillermo.heras@gmail.com>
2014-04-08 13:59:06 +0200

    Añadir origenes
    Kanon, Togarini
    re #141

--- js/data/origenes/origenes.js
+++ js/data/origenes/origenes.js
@@ -1279,3 +1279,387 @@ origen = new Origen(
 
 allOrigenes[origen.nombre] = origen;
 origenes_set.push(origen);
+
+origen = new Origen(
+    ORIGEN_KANON,
+    function() {
+        var azar = d100();
+
+        if (azar <= 82) {
+            return ETNIA_ASHER;
+        } else if (azar <= 92) {
+            return ETNIA_AION ;
+        } else if (azar <= 98) {
+            return ETNIA_VILDIAN;
+        } else if (azar <= 99) {
+            return ETNIA_DAEVAR ;
+        } else  {
+            var azar2 = d(5);
+
+            switch (azar2) {
+                case 1:
+                    return ETNIA_KWA;
+                case 2:
+                    return ETNIA_RYUAN;
+                case 3:
+                    return ETNIA_ZINNER ;
+                case 4:
+                    return ETNIA_NORNE;
+                case 5:
+                default:
+                    return ETNIA_TAYAHAR;
+            }
+        }
+    },
+    [IDIOMA_LATIN],
+    ['Alesky','Aron','Borys','Celestyn','Dawid','Ferdynand','Gustaw','Ignacy','Iwan','Janek','Kornel','Ludwik','Marek','Miloslaw','Tymon','Witold','Zygfryd'],
+    ['Ania','Edyta','Ewa','Hania','Kaja','Lidia','Malwina','Melania','Ola','Patrycja','Seweryna','Sylwia','Urszula','Waleria','Wiola','Zoja'],
+    ['Adamski','Aviza','Bielenda','Ceran','Czekaj','Derda','Dubilas','Faron','Harasym','Jaktorowo','Kalak','Nowak','Parzysz','Polek','Rawa','Tokarski','Wyrostek','Zachemski'],
+    ['Aldon','Bendek','La iglesia de San Bernabé','Castillo de Odessa','Kassia','Brudge','Berke','Reisz','Terhel'],
+    0,
+    {},
+    [
+        {
+            nombre: CLASE_BAJA,
+            rango: 10,
+            dinero: new Dinero(0,0,3),
+            bonos: [
+                {
+                    habilidad: HB_ATLETISMO,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_MONTAR,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_NADAR,
+                    bono: 30
+                },
+                {
+                    habilidad: HB_ADVERTIR,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_ANIMALES,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_HERBOLARIA,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_NAVEGACION,
+                    bono: 30
+                }
+            ]
+        },
+        {
+            nombre: CLASE_MEDIA,
+            rango: 85,
+            dinero: new Dinero(0,75,0),
+            bonos: [
+                {
+                    habilidad: HB_ATLETISMO,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_MONTAR,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_NADAR,
+                    bono: 30
+                },
+                {
+                    habilidad: HB_ADVERTIR,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_ANIMALES,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_HERBOLARIA,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_NAVEGACION,
+                    bono: 30
+                }
+            ]
+        },
+        {
+            nombre: CLASE_NOBLEZA,
+            rango: 100,
+            dinero: new Dinero(60,0,0),
+            bonos: [
+                {
+                    habilidad: HB_ATLETISMO,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_MONTAR,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_NADAR,
+                    bono: 30
+                },
+                {
+                    habilidad: HB_ADVERTIR,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_ANIMALES,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_HERBOLARIA,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_NAVEGACION,
+                    bono: 30
+                }
+            ]
+        }
+    ]
+);
+
+allOrigenes[origen.nombre] = origen;
+origenes_set.push(origen);
+
+origen = new Origen(
+    ORIGEN_TOGARINI,
+    function() {
+        var azar = d100();
+
+        if (azar <= 76) {
+            return  ETNIA_DAEVAR ;
+        } else if (azar <= 90) {
+            return ETNIA_VILDIAN;
+        } else if (azar <= 96) {
+            return ETNIA_ASHER ;
+        } else if (azar <= 99) {
+            return ETNIA_AION ;
+        } else  {
+            var azar2 = d(5);
+
+            switch (azar2) {
+                case 1:
+                    return ETNIA_KWA;
+                case 2:
+                    return ETNIA_RYUAN;
+                case 3:
+                    return ETNIA_ZINNER ;
+                case 4:
+                    return ETNIA_NORNE;
+                case 5:
+                default:
+                    return ETNIA_TAYAHAR;
+            }
+        }
+    },
+    [IDIOMA_LATIN],
+    ['Alaric','Arnold','Berg','Bert','Braun','Carl','Diedrick','Durin','Emil','Franz','Gunther','Heinrich','Herman','Kaiser','Koen','Luther','Roland','Seifer','Werner'],
+    ['Adalia','Allison','Analise','Annika','Bertha','Clove','Crescentia','Edda','Emera','Griselda','Ida','Liesel','Mariel','Marlene','Milla','Rae','Walda','Wilhelmina'],
+    ['Aachen','Adler','Barth','Berg','Eichel','Esisenberg','Fassbinder','Fiedler','Gloeckner','Hertz','Hirsch','Kalb','Mauser','Nacht','Reinhard','Roth','Sankt','Schneider','Strauss','Weiss'],
+    ['Aubrey','Wilkend','Kaine','Reindhold','Waldemar','Seifer','El Monasterio de la Sagrada Iluminación','Schank','Hausburz','Reine'],
+    1,
+    {
+        nombre: CLASE_ALTA_NOBLEZA,
+        dinero: new Dinero(900,0,0),
+        bonos: [
+            {
+                habilidad: HB_MONTAR,
+                bono: 20
+            },
+            {
+                habilidad: HB_ESTILO,
+                bono: 20
+            },
+            {
+                habilidad: HB_FRIALDAD,
+                bono: 20
+            },
+            {
+                habilidad: HB_LIDERAZGO,
+                bono: 20
+            },
+            {
+                habilidad: HB_INTIMIDAR,
+                bono: 20
+            },
+            {
+                habilidad: HB_PERSUASION,
+                bono: 20
+            },
+            {
+                habilidad: HB_HISTORIA,
+                bono: 20
+            },
+            {
+                habilidad: HB_BAILE,
+                bono: 20
+            }
+        ]
+    },
+    [
+        {
+            nombre: CLASE_BAJA,
+            rango: 10,
+            dinero: new Dinero(0,0,3),
+            bonos: [
+                {
+                    habilidad: HB_ATLETISMO,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_MONTAR,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_TREPAR,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_ADVERTIR,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_TRUCOS_MANOS,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_FRIALDAD,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_PERSUASION,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_HISTORIA,
+                    bono: 20
+                }
+            ]
+        },
+        {
+            nombre: CLASE_MEDIA,
+            rango: 60,
+            dinero: new Dinero(1,0,0),
+            bonos: [
+                {
+                    habilidad: HB_ATLETISMO,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_MONTAR,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_TREPAR,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_ADVERTIR,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_TRUCOS_MANOS,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_FRIALDAD,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_PERSUASION,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_HISTORIA,
+                    bono: 20
+                }
+            ]
+        },
+        {
+            nombre: CLASE_BURGUES,
+            rango: 90,
+            dinero: new Dinero(15,0,0),
+            bonos: [
+                {
+                    habilidad: HB_MONTAR,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_PERSUASION,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_ESTILO,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_FRIALDAD,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_HISTORIA,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_TASACION,
+                    bono: 40
+                },
+                {
+                    habilidad: HB_BAILE,
+                    bono: 20
+                }
+            ]
+        },
+        {
+            nombre: CLASE_NOBLEZA,
+            rango: 100,
+            dinero: new Dinero(120,0,0),
+            bonos: [
+                {
+                    habilidad: HB_MONTAR,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_ESTILO,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_FRIALDAD,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_LIDERAZGO,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_INTIMIDAR,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_PERSUASION,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_HISTORIA,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_BAILE,
+                    bono: 20
+                }
+            ]
+        }
+    ]
+);
+
+allOrigenes[origen.nombre] = origen;
+origenes_set.push(origen);
