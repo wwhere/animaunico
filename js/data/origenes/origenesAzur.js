6abe1915e2458e179fecabc5bebe254eab5e3c4c
Guillermo Heras <guillermo.heras@gmail.com>
2014-04-08 17:04:26 +0200

    añadir alianza azur
    re #141

--- js/data/origenes/origenesAzur.js
+++ js/data/origenes/origenesAzur.js
@@ -233,3 +233,404 @@ origen = new Origen(
 
 allOrigenes[origen.nombre] = origen;
 origenes_set.push(origen);
+
+origen = new Origen(
+    ORIGEN_REMO,
+    function() {
+        var azar = d100();
+
+        if (azar <= 71) {
+            return ETNIA_VILDIAN;
+        } else if (azar <= 93) {
+            return ETNIA_ASHER ;
+        } else if (azar <= 97) {
+            return ETNIA_DAEVAR ;
+        } else if (azar <= 99) {
+            return ETNIA_AION ;
+        } else  {
+            var azar2 = d(5);
+
+            switch (azar2) {
+                case 1:
+                    return ETNIA_KWA;
+                case 2:
+                    return ETNIA_RYUAN;
+                case 3:
+                    return ETNIA_ZINNER ;
+                case 4:
+                    return ETNIA_NORNE;
+                case 5:
+                default:
+                    return ETNIA_TAYAHAR;
+            }
+        }
+    },
+    [IDIOMA_LATIN],
+    ['Alessandro','Antonio','Carlo','Daniele','Davide','Edoardo','Enrico','Ettore','Francesco','Giacomo','Lorenzo','Mario','Paolo','Stefano','Valentino'],
+    ['Agata','Alessia','Andrea','Carmela','Chiara','Diana','Giovanna','Lucía','Michele','Rosalia','Sara','Simona','Stefanía','Teresa'],
+    ['Abbazia','Bellanti','Bianco','Corizzi','D´Agostino','Di Amato','Di Benedetto','Di Blasi','Di Gaetano','Di Luca','Dinolfo','Florio','Geraci','Marciano','Parrino','Pelligrino','Ridulfo','Scacciaferro','Vitale'],
+    ['Bastel','Albino','Caliardo','Uzure','Dedo de Hierro','Itzelhaya','Caoria','Harma','Terravecchia'],
+    0,
+    {},
+    [
+        {
+            nombre: CLASE_BAJA,
+            rango: 20,
+            dinero: new Dinero(0,0,1),
+            bonos: [
+                {
+                    habilidad: HB_ATLETISMO,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_NADAR,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_SALTAR,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_INTIMIDAR,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_ADVERTIR,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_OCULTARSE,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_ROBO,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_SIGILO,
+                    bono: 20
+                }
+            ]
+        },
+        {
+            nombre: CLASE_MEDIA,
+            rango: 80,
+            dinero: new Dinero(0,20,0),
+            bonos: [
+                {
+                    habilidad: HB_ATLETISMO,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_NADAR,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_SALTAR,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_INTIMIDAR,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_ADVERTIR,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_OCULTARSE,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_ROBO,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_SIGILO,
+                    bono: 20
+                }
+            ]
+        },
+        {
+            nombre: CLASE_BURGUES,
+            rango: 90,
+            dinero: new Dinero(5,0,0),
+            bonos: [
+                {
+                    habilidad: HB_MONTAR,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_NADAR,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_PERSUASION,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_ADVERTIR,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_FRIALDAD,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_INTIMIDAR,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_HISTORIA,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_TASACION,
+                    bono: 20
+                }
+            ]
+        },
+        {
+            nombre: CLASE_NOBLEZA,
+            rango: 100,
+            dinero: new Dinero(30,0,0),
+            bonos: [
+                {
+                    habilidad: HB_MONTAR,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_NADAR,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_PERSUASION,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_ADVERTIR,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_FRIALDAD,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_INTIMIDAR,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_HISTORIA,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_TASACION,
+                    bono: 20
+                }
+            ]
+        }
+    ]
+);
+
+allOrigenes[origen.nombre] = origen;
+origenes_set.push(origen);
+
+origen = new Origen(
+    ORIGEN_BELLAFONTE,
+    function() {
+        var azar = d100();
+
+        if (azar <= 80) {
+            return ETNIA_VILDIAN;
+        } else if (azar <= 91) {
+            return ETNIA_DAEVAR;
+        } else if (azar <= 98) {
+            return ETNIA_ASHER ;
+        } else if (azar <= 99) {
+            return ETNIA_AION ;
+        } else  {
+            var azar2 = d(5);
+
+            switch (azar2) {
+                case 1:
+                    return ETNIA_KWA;
+                case 2:
+                    return ETNIA_RYUAN;
+                case 3:
+                    return ETNIA_ZINNER ;
+                case 4:
+                    return ETNIA_NORNE;
+                case 5:
+                default:
+                    return ETNIA_TAYAHAR;
+            }
+        }
+    },
+    [IDIOMA_LATIN],
+    ['Agostino','Ángelo','Benito','Cirocco','Darío','Domani','Donato','Fabrizio','Gaetano','Giancarlo','Gustavo','Lazzaro','Maurizio','Nicola','Paolo','Umberto'],
+    ['Alda','Andrea','Benita','Camila','Cinzia','Emanuele','Gia','Luca','Luciana','Melita','Mistico','Natalia','Ornella','Paola','Rocío','Sarafina','Valeria','Viviana'],
+    ['Abatti','Accidio','Barberi','Bezzo','Cacicia','Choisio','Messina','Nicoletti','Ozello','Preatoni','Sacane','Tarsitano','Teresi','Testa','Ubertino','Zafrano'],
+    ['Raverna','Faro de Lucerna','Elisio','Cedonia','Ascani','Puerto de Ripia','Enna','Ciavolo','Musevera'],
+    1,
+    {
+        nombre: CLASE_ALTA_NOBLEZA,
+        dinero: new Dinero(800,0,0),
+        bonos: [
+            {
+                habilidad: HB_ATLETISMO,
+                bono: 20
+            },
+            {
+                habilidad: HB_MONTAR,
+                bono: 30
+            },
+            {
+                habilidad: HB_LIDERAZGO,
+                bono: 40
+            },
+            {
+                habilidad: HB_ADVERTIR,
+                bono: 10
+            },
+            {
+                habilidad: HB_FRIALDAD,
+                bono: 20
+            },
+            {
+                habilidad: HB_HISTORIA,
+                bono: 20
+            },
+            {
+                habilidad: HB_OCULTISMO,
+                bono: 20
+            },
+            {
+                habilidad: HB_BAILE,
+                bono: 20
+            }
+        ]
+    },
+    [
+        {
+            nombre: CLASE_BAJA,
+            rango: 20,
+            dinero: new Dinero(0,0,4),
+            bonos: [
+                {
+                    habilidad: HB_ATLETISMO,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_MONTAR,
+                    bono: 30
+                },
+                {
+                    habilidad: HB_NADAR,
+                    bono: 10
+                },
+                {
+                    habilidad: HB_ADVERTIR,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_FRIALDAD,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_PROEZAS_FUERZA,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_HISTORIA,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_OCULTISMO,
+                    bono: 20
+                }
+            ]
+        },
+        {
+            nombre: CLASE_MEDIA,
+            rango: 80,
+            dinero: new Dinero(0,80,0),
+            bonos: [
+                {
+                    habilidad: HB_ATLETISMO,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_MONTAR,
+                    bono: 30
+                },
+                {
+                    habilidad: HB_NADAR,
+                    bono: 10
+                },
+                {
+                    habilidad: HB_ADVERTIR,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_FRIALDAD,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_PROEZAS_FUERZA,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_HISTORIA,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_OCULTISMO,
+                    bono: 20
+                }
+            ]
+        },
+        {
+            nombre: CLASE_NOBLEZA,
+            rango: 100,
+            dinero: new Dinero(100,0,0),
+            bonos: [
+                {
+                    habilidad: HB_ATLETISMO,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_MONTAR,
+                    bono: 30
+                },
+                {
+                    habilidad: HB_LIDERAZGO,
+                    bono: 40
+                },
+                {
+                    habilidad: HB_ADVERTIR,
+                    bono: 10
+                },
+                {
+                    habilidad: HB_FRIALDAD,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_HISTORIA,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_OCULTISMO,
+                    bono: 20
+                },
+                {
+                    habilidad: HB_BAILE,
+                    bono: 20
+                }
+            ]
+        }
+    ]
+);
+
+allOrigenes[origen.nombre] = origen;
+origenes_set.push(origen);
+
