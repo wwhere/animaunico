703569e86e3cdc46b8d966cb41429eddca2ccf56
Guillermo Heras <guillermo.heras@gmail.com>
2014-04-20 19:46:23 +0200

    cambios ars magnus
    re #130

--- js/controller/ArsMagnus_ct.js
+++ js/controller/ArsMagnus_ct.js
@@ -3,10 +3,15 @@
  * @type {ArsMagnus[]}
  */
 var arsMagnus_set = [];
-
 var allArsMagnus = {};
+var arsMagnusMenores = [];
 
-function addArsMagnus(arsMagnus) {
+function addArsMagnus(arsMagnus, grupo) {
     arsMagnus_set.push(arsMagnus);
     allArsMagnus[arsMagnus.nombre] = arsMagnus;
+    switch (grupo) {
+        case GRUPO_ARS_MAGNUS_MENORES:
+            arsMagnusMenores[arsMagnus.nombre] = arsMagnus;
+            break;
+    }
 }
--- js/data/arsMagnus/ArsMagnus.js
+++ js/data/arsMagnus/ArsMagnus.js
@@ -1,2 +1,22 @@
-addArsMagnus(new ArsMagnus(ARS_MAGNUS_BERSERKER,[],30,10,GRUPO_ARS_MAGNUS_MENORES,[FLAG_DOMINUS_EXXET_ENABLED]));
-addArsMagnus(new ArsMagnus(ARS_MAGNUS_YUUSE_BATOJUTSU,[],30,10,GRUPO_ARS_MAGNUS_MENORES,[FLAG_DOMINUS_EXXET_ENABLED]));
+addArsMagnus(
+    new ArsMagnus(
+        ARS_MAGNUS_BERSERKER,
+        [],
+        30,
+        10,
+        [FLAG_DOMINUS_EXXET_ENABLED]
+    ),
+    GRUPO_ARS_MAGNUS_MENORES
+);
+addArsMagnus(
+    new ArsMagnus(
+        ARS_MAGNUS_YUUSE_BATOJUTSU,
+        [
+            new Requisito(REQUISITO_TABLA,TABLA_BATTO_JUTSU_IAI_JUTSU,0)
+        ],
+        30,
+        10,
+        [FLAG_DOMINUS_EXXET_ENABLED]
+    ),
+    GRUPO_ARS_MAGNUS_MENORES
+);
